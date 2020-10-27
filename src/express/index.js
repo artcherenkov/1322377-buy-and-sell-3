ecf3acaffeb1ec10412d3602f0beda1825d0bf82
Артем Черенков <art.cherenkov@gmail.com>
2020-10-27 21:13:04 +0700

    Добавляет рендеринг шаблонов

--- src/express/index.js
+++ src/express/index.js
@@ -1,12 +1,11 @@
 'use strict';
 const express = require(`express`);
+const path = require(`path`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
-
-app.set(`views`, `./templates`);
-app.set(`view engine`, `pug`);
 
 const myRoutes = require(`./routes/my-routes`);
 const offersRoutes = require(`./routes/offers-routes`);
@@ -16,6 +15,13 @@ app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/offers`, offersRoutes);
 
-app.get(`/`, (req, res) => res.send(`Hello, Express.js!`));
-app.listen(DEFAULT_PORT,
-    () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use((req, res) => res.status(400).render(`errors/404`));
+app.use((err, req, res) => res.status(500).render(`errors/500`));
+
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+app.listen(DEFAULT_PORT, () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
